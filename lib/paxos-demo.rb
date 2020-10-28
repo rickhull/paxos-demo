8d4b039847eed1988c3a110615a7dd9a741bf3e5
Rick Hull <rwh@localhost>
2020-10-28 00:30:38 -0400

    add Coordinator and tests

--- lib/paxos-demo.rb
+++ lib/paxos-demo.rb
@@ -13,6 +13,7 @@ module PaxosDemo
 
     def receive(msg, from)
       puts "#{@name} received #{msg} from #{from}"
+      msg
     end
 
     def to_s
@@ -37,11 +38,46 @@ module PaxosDemo
     end
   end
 
-  class UnreliableClient
+  class Coordinator < Client
+    def self.process(msg_buf)
+      hsh = {}
+      msg_buf.each { |msg|
+        hsh[msg] ||= 0
+        hsh[msg] += 1
+      }
+      mesg = nil
+      count = 0
+      hsh.each { |msg, cnt|
+        if cnt > count
+          mesg = msg
+          count = cnt
+        end
+      }
+      mesg
+    end
+
+    def initialize(name, network)
+      @msg_buf = []
+      super(name, network)
+    end
+
+    def receive(msg, from)
+      @msg_buf << msg
+      super(msg, from)
+    end
+
+    def process_msg_buf
+      maj = self.class.process(@msg_buf)
+      puts "maj = #{maj}"
+      @msg_buf = []
+    end
+  end
+
+  class UnreliableClient < Client
     FAILURE_RATE = 0.1
   end
 
-  class UnreliableNetwork
+  class UnreliableNetwork < Client
     FAILURE_RATE = 0.1
   end
 end
---
+++ test/coordinator.rb
@@ -0,0 +1,54 @@
+require 'paxos-demo'
+require 'minitest/autorun'
+
+include PaxosDemo
+
+describe Coordinator do
+  it "must find the most common member of an array" do
+    def create_ary(hsh)
+      ary = []
+      hsh.each { |k,v|
+        v.times { ary << k }
+      }
+      ary
+    end
+    
+    clear = create_ary({
+                         alice: 35,
+                         bob: 32,
+                         charlie: 30,
+                         david: 5,
+                       })
+
+    unclear = create_ary({
+                           alice: 35,
+                           bob: 35,
+                           charlie: 35,
+                           david: 30,
+                         })
+
+    reverse = create_ary({
+                           alice: 5,
+                           bob: 30,
+                           charlie: 32,
+                           david: 35,
+                         })
+
+    tied = create_ary({
+                        alice: 2,
+                        bob: 2,
+                        charlie: 2,
+                      })
+
+    expect(Coordinator.process(clear)).must_equal(:alice)
+    expect(Coordinator.process(reverse)).must_equal(:david)
+    25.times {
+      unc = Coordinator.process(unclear)
+      expect(unc).wont_equal(:david)
+      expect([:alice, :bob, :charlie]).must_include(unc)
+      ted = Coordinator.process(tied)
+      expect(ted).wont_equal(:david)
+      expect([:alice, :bob, :charlie]).must_include(ted)
+    }
+  end
+end
