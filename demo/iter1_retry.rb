2a814f61e2cac564a4b5ad0932f6d4fa12206bb7
Rick Hull <rwh@localhost>
2020-11-02 16:34:22 -0500

    add naive retries, with and without StatefulClient

---
+++ demo/iter1_retry.rb
@@ -0,0 +1,93 @@
+require 'paxos-demo'
+
+include PaxosDemo
+
+[0.0, 0.2, 0.4].each { |failure_rate|
+  puts "=" * 40
+  puts "Network failure rate: #{failure_rate}"
+  puts "=" * 40
+
+  # create a Network
+  net = Network.new('iter1', failure_rate: failure_rate)
+
+  # create 3 Coordinators
+  coords = [
+    Coordinator.new('coord0', net),
+    Coordinator.new('coord1', net),
+    Coordinator.new('coord2', net),
+  ]
+
+  # create 5 Clients with an associated structure for what they 'see'
+  client_registry = {
+    Client.new('Alice', net) => {},
+    Client.new('Bob', net) => {},
+    Client.new('Charlie', net) => {},
+    Client.new('David', net) => {},
+    Client.new('Emma', net) => {},
+  }
+
+  # each client sends a random proposal to the coordinator
+  client_registry.each { |client, reg|
+    # proposal space matches client count
+    reg[:proposal] = Random.rand(client_registry.keys.length)
+    coords.each { |coord|
+      client.send(reg[:proposal], coord)
+    }
+    # create a structure to hold coordinator responses
+    reg[:responses] = {}
+
+    # create a structure to track which coordinators have responded
+    reg[:coordinators] = []
+  }
+
+  coords.each { |coord|
+    # each coordinator selects the most popular proposal
+    puts "#{coord} has: #{coord.msgs}"
+    coord.process_msgs!
+    puts "#{coord} decided on #{coord.choice}"
+
+    # each coordinator responds to clients it has heard from
+    coord.clients.each { |client|
+      reg = client_registry[client]
+      received = coord.respond(client)
+      if received
+        reg[:responses][received] ||= 0
+        reg[:responses][received] += 1
+        reg[:coordinators] << coord
+      end
+    }
+  }
+
+  # keep retrying proposals until all coordinators have responded
+  client_registry.each { |client, reg|
+    remaining = coords - reg[:coordinators]
+    while !remaining.empty?
+      puts "=== RETRY ==="
+      remaining.each { |coord|
+        if client.send(reg[:proposal], coord)
+          received = coord.respond(client)
+          if received
+            reg[:responses][received] ||= 0
+            reg[:responses][received] += 1
+            reg[:coordinators] << coord
+          end
+        end
+      }
+      remaining = coords - reg[:coordinators]
+    end
+  }
+
+  # determine the most popular (majority?) response from the coordinators
+  client_registry.each { |client, reg|
+    maj = PaxosDemo.reduce(reg[:responses])
+    puts "#{client} has: #{reg[:responses]}; majority: #{maj}"
+    reg[:majority] = maj
+  }
+
+  agreed =
+    PaxosDemo.agreement?(*client_registry.values.map { |h| h[:majority] }) ||
+    'FAILED'
+
+  puts "Agreement: #{agreed}"
+  puts
+}
---
+++ demo/iter1_stateful_retry.rb
@@ -0,0 +1,80 @@
+require 'paxos-demo'
+
+include PaxosDemo
+
+[0.0, 0.2, 0.4].each { |failure_rate|
+  puts "=" * 40
+  puts "Network failure rate: #{failure_rate}"
+  puts "=" * 40
+
+  # create a Network
+  net = Network.new('iter1', failure_rate: failure_rate)
+
+  # create 3 Coordinators
+  coords = [
+    Coordinator.new('coord0', net),
+    Coordinator.new('coord1', net),
+    Coordinator.new('coord2', net),
+  ]
+
+  # create 5 Clients with an associated structure for what they 'see'
+  clients = [
+    StatefulClient.new('Alice', net),
+    StatefulClient.new('Bob', net),
+    StatefulClient.new('Charlie', net),
+    StatefulClient.new('David', net),
+    StatefulClient.new('Emma', net),
+  ]
+
+  # each client sends a random proposal to each coordinator
+  clients.each { |client|
+    client.proposal = Random.rand(clients.length)
+    coords.each { |coord|
+      # note: client doesn't yet know if the proposal was received
+      client.propose(coord)
+    }
+  }
+
+  coords.each { |coord|
+    # each coordinator selects the most popular proposal
+    puts "#{coord} has: #{coord.msgs}"
+    coord.process_msgs!
+    puts "#{coord} decided on #{coord.choice}"
+
+    # each coordinator responds to clients it has heard from
+    coord.clients.each { |client|
+      coord.respond(client)
+    }
+  }
+
+  # keep retrying proposals until all coordinators have responded
+  clients.each { |client|
+    # stateful client knows which coordinators have responded
+    client.coordinators.each { |coord, resp|
+      next if resp
+      puts "=== RETRY ==="
+      while !resp
+        # coordinator can only respond if the proposal is delivered
+        if client.propose(coord)
+          resp = coord.respond(client)
+        end
+      end
+    }
+  }
+
+  begin
+    majorities = []
+    clients.each { |client|
+      majorities << client.majority
+      puts "#{client}: #{majorities.last} #{client.coordinators.values}"
+    }
+
+    puts ["Agreement", PaxosDemo.agreement?(*majorities) || 'FAILED'
+         ].join(': ')
+    puts
+  rescue RuntimeError => e
+    puts e.message
+    puts "FAILED"
+    puts
+  end
+}
