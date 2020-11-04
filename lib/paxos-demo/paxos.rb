5c6ab8991d91950e4762939c7e70704952d2bf16
Rick Hull <rwh@localhost>
2020-11-03 19:43:01 -0500

    minor reorg

--- lib/paxos-demo/paxos.rb
+++ lib/paxos-demo/paxos.rb
@@ -1,4 +1,23 @@
 module PaxosDemo
+  class Message
+    PREPARE_MAGIC_VALUE = :prepare
+
+    attr_reader :number, :value
+
+    def initialize(number: 0, value: PREPARE_MAGIC_VALUE)
+      @number = number
+      @value = value
+    end
+
+    def prepare?
+      @value == PREPARE_MAGIC_VALUE
+    end
+
+    def to_s
+      [@number, @value].join(':')
+    end
+  end
+
   class Proposer
     class ProposalError < RuntimeError; end
 
@@ -31,25 +50,6 @@ module PaxosDemo
     end
   end
 
-  class Message
-    PREPARE_MAGIC_VALUE = :prepare
-
-    attr_reader :number, :value
-
-    def initialize(number: 0, value: PREPARE_MAGIC_VALUE)
-      @number = number
-      @value = value
-    end
-
-    def prepare?
-      @value == PREPARE_MAGIC_VALUE
-    end
-
-    def to_s
-      [@number, @value].join(':')
-    end
-  end
-
   class Acceptor
     attr_reader :prepare, :proposal
 
@@ -65,20 +65,20 @@ module PaxosDemo
           @prepare = request
           return @proposal ? @proposal : @prepare
         else
-          puts "got a negative prepare request"
           # don't respond, or respond negatively
+          puts "got a negative prepare request"
         end
       else # this is a proposal
         if @prepare.nil?
-          puts "got a proposal but haven't seen any prepares yet"
           # this is probably an error
+          puts "got a proposal but haven't seen any prepares yet"
         elsif request.number >= @prepare.number
           puts "got a positive proposal"
           @proposal = request
           return @proposal
         else
-          puts "got a negative proposal"
           # don't respond, or respond negatively
+          puts "got a negative proposal"
         end
       end
     end
